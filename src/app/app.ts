fbf14fab70e3375e35abfbe23b94e0d4cee5b877
Marimonov <vc_khti@mail.ru>
2025-11-13 15:34:55 +0700

    Вывел миниатюры на страницу

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,41 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {CommonModule, IMAGE_CONFIG, NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NgOptimizedImage,
+    CommonModule
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  providers: [
+    {
+      provide: IMAGE_CONFIG,
+      useValue: {
+        disableImageSizeWarning: true,
+        disableImageLazyLoadWarning: true
+      }
+    },
+  ]
 })
 export class App {
   protected readonly title = signal('photo');
+  items: number [] = [];
+  anotherItems: number [] = [];
+
+  constructor() {
+    for (let i = 1; i <= 35; i++) {
+      this.items.push(i);
+    }
+
+    for (let i = 35; i <= 83; i++) {
+      this.anotherItems.push(i);
+    }
+    console.log('items', this.items);
+  }
+
+
 }
