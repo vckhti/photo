7628481d39e6df5c60ab73bfe2c14873499159de
Marimonov <vc_khti@mail.ru>
2025-11-13 16:40:10 +0700

    Прикрутил слайдер

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
-import {Component, signal} from '@angular/core';
+import {Component, signal, ViewChild} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {CommonModule, IMAGE_CONFIG, NgOptimizedImage} from '@angular/common';
+import {ImageObject, RmImageSliderComponent} from 'rm-image-slider';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     NgOptimizedImage,
+    RmImageSliderComponent,
     CommonModule
   ],
   templateUrl: './app.html',
@@ -25,17 +27,67 @@ export class App {
   protected readonly title = signal('photo');
   items: number [] = [];
   anotherItems: number [] = [];
+  @ViewChild('nav') slider: any;
+  any: any[] = [];
+
+  imageObject: Array<ImageObject> = [
+    {
+    image: 'assets/images/1.jpg',
+    thumbImage: 'assets/images/1.jpeg',
+    alt: 'alt of image',
+    title: '1',
+    index: 1
+  },
+    {
+      image: 'assets/images/2.jpg',
+      thumbImage: 'assets/images/2.jpeg',
+      alt: 'alt of image',
+      title: '2',
+      index: 2
+    },
+    {
+      image: 'assets/images/3.jpg',
+      thumbImage: 'assets/images/3.jpeg',
+      alt: 'alt of image',
+      title: '3',
+      index: 3
+    },
+  ];
 
   constructor() {
-    for (let i = 1; i <= 35; i++) {
-      this.items.push(i);
+    for (let i = 1; i <= 34; i++) {
+      this.items.push(i)
+/*      this.any.push(
+        {
+          image: 'assets/images/1.jpg',
+          thumbImage: 'assets/images/1.jpeg',
+          alt: 'alt of image',
+          title: `${i-1}`,
+          index: i
+        }
+      );
+      this.imageObject = this.any;*/
     }
 
-    for (let i = 35; i <= 83; i++) {
+
+    for (let i = 36; i <= 83; i++) {
+
       this.anotherItems.push(i);
     }
-    console.log('items', this.items);
+
   }
 
+  prevImageClick() {
+    this.slider.prev();
+  }
+
+  sliderOpen(index: number) {
+    console.log('index', index);
+    this.slider.imageOnClick(index);
+  }
+
+  nextImageClick() {
+    this.slider.next();
+  }
 
 }
